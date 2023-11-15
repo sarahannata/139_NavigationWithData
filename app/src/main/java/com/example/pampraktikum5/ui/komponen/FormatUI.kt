50e62f77ee46d755788451c5dfbfb5e77bec79df
sarahannata <sarahannata@gmail.com>
2023-11-16 00:52:32 +0700

    add FormatDataPemesanan to FormatUI

--- app/src/main/java/com/example/pampraktikum5/ui/komponen/FormatUI.kt
+++ app/src/main/java/com/example/pampraktikum5/ui/komponen/FormatUI.kt
@@ -1,10 +1,14 @@
 package com.example.pampraktikum5.ui.komponen
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.pampraktikum5.R
 
 @Composable
@@ -14,4 +18,34 @@ fun FormatLabelHarga(subtotal: String, modifier : Modifier = Modifier){
         modifier = modifier,
         style = MaterialTheme.typography.headlineSmall
     )
+}
+@Composable
+fun FormatDataPemesanan(
+    namaPemesanan: String,
+    alamatPemesan: String,
+    phonePemesan: String,
+){
+    Text(
+        text = stringResource(R.string.namaOrder),
+    )
+    Text(namaPemesanan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+
+    Text(
+        text = stringResource(R.string.alamatOrder),
+    )
+    Text(alamatPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(10.dp))
+
+
+    Text(
+        text = stringResource(R.string.phoneOrder),
+    )
+    Text(phonePemesan)
+    Divider()
+
+
 }
