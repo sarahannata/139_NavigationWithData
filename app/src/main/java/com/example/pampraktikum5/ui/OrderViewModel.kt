c2e44ae1d574de7bac1f0cebe92fe8a3e31d2876
sarahannata <sarahannata@gmail.com>
2023-11-16 00:15:01 +0700

    add fun setNama

--- app/src/main/java/com/example/pampraktikum5/ui/OrderViewModel.kt
+++ app/src/main/java/com/example/pampraktikum5/ui/OrderViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pampraktikum5.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.pampraktikum5.data.FormState
 import com.example.pampraktikum5.data.OrderUIState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,7 +13,16 @@ private const val HARGA_PER_CUP = 3000
 
 class OrderViewModel : ViewModel() {
     private val _stateUI = MutableStateFlow(OrderUIState())
+    private val _nameSTATE = MutableStateFlow(FormState())
     val stateUI:StateFlow<OrderUIState> = _stateUI.asStateFlow()
+    val nameST : StateFlow<FormState> = _nameSTATE.asStateFlow()
+    fun setNama(list: MutableList<String>){
+        _nameSTATE.update { stateSaatIni -> stateSaatIni.copy(
+            nama = list[0],
+            alamat = list[1],
+            phone = list[2]
+        ) }
+    }
 
     fun setJumlah(jmlEsJumbo:Int){
         _stateUI.update { stateSaatIni ->
