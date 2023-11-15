1051069516f7094e734d3e33ecbc9aa719a93ed1
sarahannata <sarahannata@gmail.com>
2023-11-16 00:08:01 +0700

    Add File HalamanForm and add var into it

---
+++ app/src/main/java/com/example/pampraktikum5/ui/theme/HalamanForm.kt
@@ -0,0 +1,44 @@
+package com.example.activity5
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pampraktikum5.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonCLicked: () -> Unit
+){
+
+    var namaText by remember {
+        mutableStateOf("")
+    }
+    var alamatText by remember {
+        mutableStateOf("")
+    }
+    var nomortlpText by remember {
+        mutableStateOf("")
+    }
+}
